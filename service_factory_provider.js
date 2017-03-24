7c0992fb4b29a8a20fe20fa15b930df47cc344d8
Shailendra Pathak <shailendra9@users.noreply.github.com>
2017-03-24 11:38:46 +0530

    Update service_factory_provider.js

--- service_factory_provider.js
+++ service_factory_provider.js
@@ -1,50 +1,5 @@
-var myApp = angular.module('myApp', []);
+you can make the separate js code here which i have written in html file.and include the path of  this js file  into html file.
 
-//service style, probably the simplest one
-myApp.service('helloWorldFromService', function() {
-    this.sayHello = function() {
-        return "Hello, World!"
-    };
-});
 
-//factory style, more involved but more sophisticated
-myApp.factory('helloWorldFromFactory', function() {
-    return {
-        sayHello: function() {
-            return "Hello, World!"
-        }
-    };
-});
-    
-//provider style, full blown, configurable version     
-myApp.provider('helloWorld', function() {
-
-    this.name = 'Default';
-
-    this.$get = function() {
-        var name = this.name;
-        return {
-            sayHello: function() {
-                return "Hello, " + name + "!"
-            }
-        }
-    };
-
-    this.setName = function(name) {
-        this.name = name;
-    };
-});
-
-//hey, we can configure a provider!            
-myApp.config(function(helloWorldProvider){
-    helloWorldProvider.setName('World');
-});
-        
-
-function MyCtrl($scope, helloWorld, helloWorldFromFactory, helloWorldFromService) {
-    
-    $scope.hellos = [
-        helloWorld.sayHello(),
-        helloWorldFromFactory.sayHello(),
-        helloWorldFromService.sayHello()];
-}
+Thanks
+Shailendra
